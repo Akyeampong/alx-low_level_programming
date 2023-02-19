a9cbb6edbb4bbd3e2c7b0632c8d56ad3bc1a6448
Akyeampong <akyeampongkwaku30@gmail.com>
2023-02-19 06:28:37 -0800

    alpha 11

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Alphabets from a to z
- * 
- * Return: Always 0 (Success)
+ * main - Prints alphabet in lowercase.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	char lo;
 
 	for (lo = 'a'; lo <= 'z'; lo++)
 		putchar(lo);
+
 	putchar('\n');
 
 	return (0);
