3cd96840db4129728d14539e0981cdce3b89840b
Akyeampong <akyeampongkwaku30@gmail.com>
2023-02-19 06:44:39 -0800

    upper lower1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char let;
 
 	for (let = 'a'; let <= 'z'; let++)
 		putchar(let);
-
 	for (let = 'A'; let <= 'Z'; let++)
-                putchar(let);
-
+		putchar(let);
 	putchar('\n');
 
 	return (0);
