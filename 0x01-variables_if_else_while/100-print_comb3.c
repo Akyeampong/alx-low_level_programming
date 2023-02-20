4a39910e36b0ac4f1fb2a0abe9e8bf950d1771a2
Akyeampong <akyeampongkwaku30@gmail.com>
2023-02-20 07:06:00 -0800

    double2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int dnum1, dnum2;
+	int dnum1, dnum;
 
 	for (dnum1 = 0; dnum1 < 9; dnum1++)
 	{
-		for (dnum2 = dnum1 + 1; dnum2 < 10; dnum2++)
+		for (dnum = dnum1 + 1; dnum < 10; dnum++)
 		{
 			putchar((dnum1 % 10) + '0');
-			putchar((dnum2 & 10) + '0');
+			putchar((dnum & 10) + '0');
 
-			if (dnum1 == 8 && dnum2 == 9)
+			if (dnum1 == 8 && dnum == 9)
 				continue;
 
 			putchar(',');
