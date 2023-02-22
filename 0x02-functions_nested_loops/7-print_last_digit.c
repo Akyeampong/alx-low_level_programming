540ceed31824436bc3503f476be769341866c750
Akyeampong <akyeampongkwaku30@gmail.com>
2023-02-22 09:58:45 -0800

    last num3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,7 @@
  * print_last_digit - prints the last digit of a number
  * @i: testing character
  *
- * Return - The value of the last digit
+ * Return - last_digit
  */
 int print_last_digit(int i)
 {
