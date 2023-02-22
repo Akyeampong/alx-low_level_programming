7a622a9db8d25e13586d3cb212d8c23b0a6956b8
Akyeampong <akyeampongkwaku30@gmail.com>
2023-02-22 04:26:08 -0800

    10x alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,16 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet in lowercase, followed by a line.
- *
- * Return: Nothing
  */
 void print_alphabet_x10(void)
 {
 	int count = 0;
 	char alpha;
 
-	while (count++ < 10)
+	while (count++ <= 9)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
 		_putchar('\n');
 	}
-}	
+}
