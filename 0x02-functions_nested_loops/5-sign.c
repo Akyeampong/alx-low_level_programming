25d38d9ea143cee4c9d04bfa001d02bd79a2f5f9
Akyeampong <akyeampongkwaku30@gmail.com>
2023-02-22 06:34:51 -0800

    sign56

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/** 
+/**
  * print_sign - prints the sign of a number
  * @n: character to be tested
  *
  * Return: 1 for a number > 0, 0 for a number = 0, -1 for a number < 0
- */ 
+ */
 int print_sign(int n)
 {
 	if (n > 0)
