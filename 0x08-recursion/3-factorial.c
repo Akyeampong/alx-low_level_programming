67a39412c9c554102ec393a71272e63a5ea87c04
Akyeampong <akyeampongkwaku30@gmail.com>
2023-03-08 22:00:45 -0800

    updates

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 
 /**
  * factorial - returns the factorial of a given number
- * @n number to calculate factorial
+ * @n: number to calculate factorial
  *
  * Return: Integer value
  */
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x integer to be worked on
- * @y integer to be worked on
- *
- * Return: Integer value
- */
+  * _pow_recursion - Returns the value of x raised to the power of y
+  * @x: the value to multiply
+  * @y: the times to multiply the value
+  *
+  * Return: the value multiplied y times
+  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)	
-		return(-1);
+	if (y < 0)
+		return (-1);
 
 	if (y == 0)
-		return(1);
+		return (1);
 
-	return(x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
